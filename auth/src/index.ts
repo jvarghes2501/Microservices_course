99caa5f12435cab4f21338cbd7687a43d97c2e74
Jestin Varghese Cherian <jvcboy251@gmail.com>
2025-04-05 13:25:23 -0400

    added error handling framework
    added error handling library that will return a strict error response structure from all the services that will be implemented in the future

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
-import express from "express";
-import { json } from "body-parser";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
 const app = express();
 app.use(json());
 
@@ -12,7 +15,13 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+  console.log(`Undefined route accessed: ${req.url}`);
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!!!");
-  console.log("This is a test");
+  console.log('Server is running on port 3000!!!');
+  console.log('This is a test');
 });
